6776691f2b76b4b6f8d844aefdba2bf31aba2f69
Yessica Widmann <yessica.widmann@hotmail.com>
2019-10-23 15:18:30 +0200

    vorgelegtes Grundgerüst

--- src/page-overview/page-overview.js
+++ src/page-overview/page-overview.js
@@ -16,17 +16,48 @@ class PageOverview {
      * Seite anzeigen. Wird von der App-Klasse aufgerufen.
      */
     async show() {
-        // TODO: Seite anzeigen
-        //this._app.database.getAllRecords();
+        // Anzuzeigenden Seiteninhalt nachladen
+        let html = await fetch("page-overview/page-overview.html");
+        let css = await fetch("page-overview/page-overview.css");
 
-        let mainElement = document.getElementById("app-main-are");
-        mainElement.innerHTML = "<button id='test-button'>Test: Bitte anklicken...</button>";
+        if (html.ok && css.ok) {
+            html = await html.text();
+            css = await css.text();
+        } else {
+            console.error("Fehler beim Laden des HTML/CSS-Inhalts");
+            return;
+        }
 
-        let testButton = document.getElementById("test-button")
-        testButton.addEventListener("click", this.onTestButtonClicked);
+        // Seite zur Anzeige bringen
+        let pageDom = document.createElement("div");
+        pageDom.innerHTML = html;
+
+        this._renderBoatTiles(pageDom);
+
+        this._app.setPageTitle("Startseite");
+        this._app.setPageCss(css);
+        this._app.setPageHeader(pageDom.querySelector("header"));
+        this._app.setPageContent(pageDom.querySelector("main"));
     }
 
-    onTestButtonClicked(){
-        alert("Test bestanden. Schönen Feierabend!");
+    /**
+     * Hilfsmethode, welche den HTML-Code zur Darstellung der Kacheln auf
+     * der Startseite erzeugt.
+     *
+     * @param {HTMLElement} pageDom Wurzelelement der eingelesenen HTML-Datei
+     * mit den HTML-Templates dieser Seite.
+     */
+    _renderBoatTiles(pageDom) {
+        let mainElement = pageDom.querySelector("main");
+        let templateElement = pageDom.querySelector("#template-tile");
+
+        this._app.database.getAllRecords().forEach(boat => {
+            let html = templateElement.innerHTML;
+            html = html.replace("{HREF}", `#/Detail/${boat.id}`);
+            html = html.replace("{IMG}", boat.img);
+            html = html.replace("{NAME}", boat.name);
+
+            mainElement.innerHTML += html;
+        });
     }
 }
