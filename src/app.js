2a304242db76a8ed69b61ac90a094e0c831f7029
dtenten <o10370236@nwytg.net>
2019-11-16 19:26:19 +0100

    Reorganised overview Page after new Design Decision
    Removed Slideshow and ordered Recipes as Tiles next to eachother. 
    Work done in collaboration with Laurent

--- src/app.js
+++ src/app.js
@@ -49,8 +49,8 @@ class App {
 
         // Single Page Router starten und die erste Seite aufrufen
         window.addEventListener("hashchange", () => this._handleRouting());
-        this._handleRouting();
         await this.database.getAllRecipes();
+        this._handleRouting();
     }
 
     /**
@@ -102,9 +102,20 @@ class App {
      */
     _handleRouting() {
         let pageUrl = location.hash.slice(1);
+        let id = "";
 
         if (pageUrl.length === 0) {
             pageUrl = "/";
+        }
+
+        if (pageUrl.split("/")[1] === "Detail") {
+            id = pageUrl.split("/")[2];
+            window.location.hash = "/";
+        }
+
+        if(pageUrl.split("/")[1] === "Favorites" && pageUrl.split("/").length === 3){
+            id = pageUrl.split("/")[2];
+            window.location.hash = "/";
         }
 
         let matches = null;
@@ -117,6 +128,10 @@ class App {
 
         this._currentPageObject = new page.klass(this);
         this._currentPageObject.show(matches);
+
+        if(id !== ""){
+            this.setModalContent(id);
+        }
     }
 
     /**
@@ -236,6 +251,12 @@ class App {
         let headline = document.getElementById("modal-headline");
         let modalBody = document.getElementById("modal-body");
         let modalFooter = document.getElementById("modal-footer");
+
+        //Sicherstellen, dass Modal leer ist
+        if (modal.classList.contains("erstellen")) {
+            modal.classList.remove("erstellen");
+        }
+        this.closeModal();
 
         //Close-Button
         let close = document.getElementsByClassName("close")[0];
@@ -491,19 +512,22 @@ class App {
                 while (modalBody.firstChild) {
                     modalBody.removeChild(modalBody.firstChild);
                 }
+                modalBody.innerHTML = "";
             }
         } else {
             modal.style.display = "none";
 
-            while (modalBody.firstChild) {
+            while (modalBody.childNodes.length > 0) {
                 modalBody.removeChild(modalBody.firstChild);
             }
+
+            modalBody.innerHTML = "";
         }
     }
 
     //Fehlerhaft!!
     imageSelection(event) {
-        if(this.value !== ""){
+        if (this.value !== "") {
             document.getElementById("imgPrev").setAttribute("src", this.value);
         }
     }
@@ -512,8 +536,8 @@ class App {
         if (this.childNodes[this.childNodes.length - 1].childNodes[0].value !== "") {
             if (e.keyCode === 13) {
             } else {
-                for(let i = 0; i < this.childNodes.length; i++){                    
-                    if(this.childNodes[i].childNodes[0].value.trim() === "") {
+                for (let i = 0; i < this.childNodes.length; i++) {
+                    if (this.childNodes[i].childNodes[0].value.trim() === "") {
                         this.removeChild(this.childNodes[i]);
                     }
                 }
@@ -527,16 +551,16 @@ class App {
                 this.appendChild(ingredientLi);
             }
         } else {
-            if (e.keyCode === 13) {                
+            if (e.keyCode === 13) {
                 this.childNodes[this.childNodes.length - 1].childNodes[0].focus();
             }
         }
     }
- 
+
     modalSubmit(e) {
         console.log("Test");
-        
-        
+
+
 
     }
 
